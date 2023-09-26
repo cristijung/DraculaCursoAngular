de7d87a9407c29621b3c35f6061f03b4f35604ce
Cristina Jung <cristijung@gmail.com>
2023-09-26 13:25:50 -0300

    ajuste estilização home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  title='Resumo | Drácula de Bram Stoker'
+  url= '../../../assets/drc.png';
+  alt='Drácula'
+
 
 }
