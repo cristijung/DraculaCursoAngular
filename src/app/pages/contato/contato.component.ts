721055bb4d44a46015840cd07e1a0d2c1f76d2f6
Cristina Jung <cristijung@gmail.com>
2023-09-28 10:35:36 -0300

    contato pronto

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -8,15 +9,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 
 export class ContatoComponent {
+  //título do texto
+  title = 'Entre em contato conosco!';
+  dados: any;
   messageForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+
+  //duas propriedades no construtor - explicar
+  constructor(private formBuilder: FormBuilder, private dataService: DataService) {
     this.messageForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       message: ['', [Validators.required]]
     });
   }
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((data) => {
+      this.dados = data;
+    });
+  }
+
 
   onSubmit() {
     if (this.messageForm.valid) {
