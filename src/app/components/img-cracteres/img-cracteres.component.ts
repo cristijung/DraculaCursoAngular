7e03c166af65526b6304299cb79b8bcd3d6f82bd
Cristina Jung <cristijung@gmail.com>
2023-09-28 14:05:39 -0300

    two-way db

--- src/app/components/img-cracteres/img-cracteres.component.ts
+++ src/app/components/img-cracteres/img-cracteres.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { personagensData } from 'src/app/components/img-cracteres/personagens-data';
 
 @Component({
   selector: 'app-img-cracteres',
@@ -10,17 +11,11 @@ export class ImgCracteresComponent {
   title = 'Principais personagens';
 
 
-  imageUrl: string = ''; // Propriedade para armazenar o URL da imagem
-  shouldDisplay: boolean = false; // Propriedade para controlar a exibição da imagem
-
-  // Função chamada quando o URL da imagem é alterado
-  onImageUrlChange() {
-    // Quando o URL da imagem é alterado, ocultamos a imagem
-    this.shouldDisplay = false;
-  }
-
-  // Função para alternar a exibição da imagem
-  toggleImage() {
-    this.shouldDisplay = !this.shouldDisplay;
+  //esta opção é para trabalhar com two-way data binding
+  @Input() nomePersonagem: string = ''; // aqui vai a entrada para o nome do personagem
+  // Função para obter o URL da imagem com base no nome do personagem
+  getImagemUrl(): string {
+    const personagem = personagensData.find(p => p.nome === this.nomePersonagem);
+    return personagem ? personagem.imagemUrl : '';
   }
 }
---
+++ src/app/components/img-cracteres/personagens-data.ts
@@ -0,0 +1,33 @@
+//base de imgs para personagens
+
+export const personagensData = [
+  {
+    nome: 'Drácula',
+    imagemUrl: '../../../assets/personagens/dracula.png',
+  },
+  {
+    nome: 'Mina',
+    imagemUrl: '../../../assets/personagens/mina.png',
+  },
+  {
+    nome: 'Van Helsing',
+    imagemUrl: '../../../assets/personagens/helsing.png',
+  },
+  {
+    nome: 'Lucy',
+    imagemUrl: '../../../assets/personagens/lucy.png',
+  },
+  {
+    nome: 'Jonathan',
+    imagemUrl: '../../../assets/personagens/jonathan.png',
+  },
+  {
+    nome: 'Arthur',
+    imagemUrl: '../../../assets/personagens/arthur.png',
+  },
+  {
+    nome: 'Cary',
+    imagemUrl: '../../../assets/personagens/cary.png',
+  },
+
+];
