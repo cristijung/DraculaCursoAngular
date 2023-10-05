e4f0b325ca99b2fa275327b6dd639b14df2906bb
Cristina Jung <cristijung@gmail.com>
2023-10-05 19:12:35 -0300

    formulario reativo

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
@@ -9,16 +8,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 
 export class ContatoComponent {
-  //título do texto
   title = 'Entre em contato conosco!';
   dados: any;
   messageForm: FormGroup;
 
-
-  //duas propriedades no construtor - explicar
   constructor(private formBuilder: FormBuilder, private dataService: DataService) {
     this.messageForm = this.formBuilder.group({
-      name: ['', [Validators.required]],
+      name: ['', [Validators.required, this.customNameValidator]],
       email: ['', [Validators.required, Validators.email]],
       message: ['', [Validators.required]]
     });
@@ -30,19 +26,27 @@ export class ContatoComponent {
     });
   }
 
+  customNameValidator(control: FormControl) {
+    const name = control.value;
+    const pattern = /^[a-zA-Z\s]{3,}$/; // Apenas letras e mínimo de 3 caracteres
+    if (!pattern.test(name)) {
+      return { invalidName: true };
+    }
+    return null;
+  }
 
   onSubmit() {
     if (this.messageForm.valid) {
       const name = this.messageForm.get('name')?.value;
       const email = this.messageForm.get('email')?.value;
       const message = this.messageForm.get('message')?.value;
-      // aqui --- configuração de envio para qualquer lugar
+      // Aqui você pode adicionar a lógica para enviar os dados para um endpoint de captação de dados
       console.log('Nome:', name);
       console.log('Email:', email);
       console.log('Mensagem:', message);
       this.messageForm.reset();
     }
   }
-
-
 }
+
+
